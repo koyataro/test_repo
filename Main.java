fe2873fa4b1c02f0e44fe6fdc0751ebdb1fe292c
koyataro <koyacchikochan@gmail.com>
2024-01-15 19:24:38 +0900

    マイPC でadd、commit,するテスト

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
 		int num;
-		for(int i=0; i<10; i++) {
+		for(int i=0; i<11; i++) {
 			num += i;
 		}
+		System.out.println("1+2+3+...+9+10 = "+num);
+		System.out.println("Hello, world.");
 	}
 }
